66f7c0ce928f85120fa72ec8c6360c3869a52212
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-12 10:45:18 -0700

    working list test

--- src/List.test.js
+++ src/List.test.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom';
 import List from './List';
 import Card from './Card';
 import renderer from 'react-test-renderer';
+import STORE from './STORE';
 
 it('renders without crashing', () =>{
   const div = document.createElement('div');
-  ReactDOM.render(<List />, div);
+  const testing = STORE.allCards.a;
+  ReactDOM.render(<List cards={[{id: 1, title: 'card 1', content: 'look heres some content'}]}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-// it('renders the UI as expected', () => {
-//   const tree = renderer
-//     .create(<List />)
-//     .toJSON();
-//   expect(tree).toMatchSnapshot();  
-//   });
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create(<List />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();  
+  });
